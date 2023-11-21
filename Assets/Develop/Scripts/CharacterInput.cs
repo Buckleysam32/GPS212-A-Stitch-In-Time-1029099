87e153f8b2c6e5b863d61db85f9a686338058359
Sam Buckley <1029099@student.sae.edu.au>
2023-11-21 13:14:44 +1000

    Added constraints to player's movement

--- Assets/Develop/Scripts/CharacterInput.cs
+++ Assets/Develop/Scripts/CharacterInput.cs
@@ -14,40 +14,41 @@ public class CharacterInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         if (onlyCardinal)
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) && transform.position.z != 0)
             {
                 transform.position += new Vector3(0, 0, 1);
             }
-            else if (Input.GetKeyDown(KeyCode.S))
+            else if (Input.GetKeyDown(KeyCode.S) && transform.position.z != -7)
             {
                 transform.position += new Vector3(0, 0, -1);
             }
-            else if (Input.GetKeyDown(KeyCode.A))
+            else if (Input.GetKeyDown(KeyCode.A) && transform.position.x != 0)
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
-            else if (Input.GetKeyDown(KeyCode.D))
+            else if (Input.GetKeyDown(KeyCode.D) && transform.position.x != 7)
             {
                 transform.position += new Vector3(1, 0, 0);
             }
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W) && transform.position.z != 0)
             {
                 transform.position += new Vector3(0, 0, 1);
             }
-            if (Input.GetKeyDown(KeyCode.S))
+            if (Input.GetKeyDown(KeyCode.S) && transform.position.z != -7)
             {
                 transform.position += new Vector3(0, 0, -1);
             }
-            if (Input.GetKeyDown(KeyCode.A))
+            if (Input.GetKeyDown(KeyCode.A) && transform.position.x != 0)
             {
                 transform.position += new Vector3(-1, 0, 0);
             }
-            if (Input.GetKeyDown(KeyCode.D))
+            if (Input.GetKeyDown(KeyCode.D) && transform.position.x != 7)
             {
                 transform.position += new Vector3(1, 0, 0);
             }
